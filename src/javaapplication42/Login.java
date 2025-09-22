2c384ae0b06fadc1580ff56bed861055cd52640f
name <email>
2025-09-21 20:22:33 -0500

    loginp

---
+++ src/javaapplication42/Login.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication42;
+
+/**
+ *
+ * @author ferta
+ */
+public class Login {
+    
+     public String saludar(String usuario) {
+        return "Bienvenido " + usuario  ;
+    }
+    
+    }
